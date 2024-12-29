7d153d4800811ffd8a0128b9b4637127f081b7fd
Emiliano Montes <101071655+louieliet@users.noreply.github.com>
2024-12-28 18:41:40 -0600

    fix: grid and plant bugs height

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,20 +9,18 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public float gravity = -9.81f;
 
-    private CharacterController characterController;
+    private Rigidbody rb;
     private Vector3 velocity;
     private Vector2 inputMovement;
     private bool isGrounded;
 
     private void Awake()
     {
-        characterController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        // Comprobar si el jugador está en el suelo
-        isGrounded = characterController.isGrounded;
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f; // Pequeña fuerza hacia abajo para mantener al jugador pegado al suelo
@@ -30,11 +28,7 @@ public class PlayerMovement : MonoBehaviour
 
         // Movimiento del personaje
         Vector3 move = new Vector3(inputMovement.x, 0, inputMovement.y);
-        characterController.Move(move * moveSpeed * Time.deltaTime);
-
-        // Aplicar gravedad
-        velocity.y += gravity * Time.deltaTime;
-        characterController.Move(velocity * Time.deltaTime);
+        rb.MovePosition(rb.position + move * moveSpeed * Time.deltaTime);
     }
 
     // Input del movimiento
